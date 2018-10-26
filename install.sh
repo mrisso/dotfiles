73507c99b4a0b329d3e3fadfa9e9681024c75a6a
Matheus H. Risso <matheush.risso@gmail.com>
2018-10-26 19:21:08 -0300

    Funções de Assembly / Relative Line Numbers retirado para a utilização de função interna do emacs26

--- install.sh
+++ install.sh
@@ -2,13 +2,15 @@
 # Copy the files to .emacs.d
 
 python -mplatform | grep -qi Ubuntu && sudo apt-get install texinfo || HOMEBREW_NO_AUTO_UPDATE=1 brew install texinfo
+python -mplatform | grep -qi Ubuntu && sudo apt-get install poppler-utils || HOMEBREW_NO_AUTO_UPDATE=1 brew install poppler-utils 
 
 EMACS_DIR="~/.emacs.d"
 CEDET_DIR="~/.emacs.d/cedet"
 
-if [ -d "$EMACS_DIR" ]; then
+if [ ! -d "$EMACS_DIR" ]; then
     echo "Creating .emacs.d/ ..."
     mkdir ~/.emacs.d/
+    cp asmRef.pdf ~/.emacs.d/
 fi
 
 cp init.el ~/.emacs.d/
@@ -16,7 +18,7 @@ cp configuration.org ~/.emacs.d/
 
 # Installing CEDET
 
-if [ -d "$CEDET_DIR" ]; then
+if [ ! -d "$CEDET_DIR" ]; then
     echo "Downloading CEDET ..."
     git clone http://git.code.sf.net/p/cedet/git ~/.emacs.d/cedet
 fi
